b61b6af1a667082fa6be656804d0f43205101688
pierelucas <pierelucas@keemail.me>
2019-10-12 22:12:58 +0200

    adding random salt function v 1.5

--- main.py
+++ main.py
@@ -34,13 +34,14 @@ import os, sys, subprocess, time
 from getpass import getpass
 from re import findall, sub
 from colorama import Fore, Style
+from Crypto import Random
 from Crypto.Cipher import AES
 from base64 import b64encode, b64decode
 import hashlib
 
 class PyStegano():
 
-    def __init__(self, salt='SlTKeYOpHygTYkP3'):
+    def __init__(self):
 
         # Time
         self.lt = time.localtime()
@@ -53,7 +54,7 @@ class PyStegano():
          |     ___<   |  |\_____  \\   __\/ __ \ / ___\__  \  /    \ /  _ \ 
          |    |    \___  |/        \|  | \  ___// /_/  > __ \|   |  (  <_> )
          |____|    / ____/_______  /|__|  \___  >___  (____  /___|  /\____/ 
-           v.1.3   \/            \/           \/_____/     \/     \/        
+           v.1.4   \/            \/           \/_____/     \/     \/        
            
                     Coded by PiereLucas | github.com/pierelucas
            """
@@ -62,13 +63,16 @@ class PyStegano():
                     [1] Write
                     [2] Read
                     [Else] Exit
+                    
+                    WARNING:
+                    [99] Generate new Salt.pl
                     """
 
         # Path
         self.path = None
 
         # Enc Dec
-        self.salt = salt
+        self.salt = None
         self.enc_dec_meth0 = 'utf-8'
         self.message = None
         self.ciphertext = None
@@ -82,38 +86,57 @@ class PyStegano():
 
     def input(self):
 
-        # Make Choice
+        choice = str(input(self.time_hm + Fore.GREEN + " [+] Which option Number » "))
+        print(Style.RESET_ALL)
+        # Gen Salt
+        if choice == '99': return None, None, None, True
+        # Path
         while True:
-            choice = str(input(self.time_hm + Fore.GREEN + " [+] Which option Number » "))
+            self.path = str(input(self.time_hm + Fore.GREEN + " [+] File Path » "))
             print(Style.RESET_ALL)
-            # Path
-            while True:
-                self.path = str(input(self.time_hm + Fore.GREEN + " [+] File Path » "))
-                print(Style.RESET_ALL)
-                if os.path.isfile(self.path): break
-                else:
-                    print(self.time_hm + Fore.RED + " [-] File is not readable")
-                    print(Style.RESET_ALL)
-                    continue
-            # Passphrase for encryption
-            while True:
-                key = str(getpass(self.time_hm + Fore.GREEN + " [+] Wrote down your Passphrase for Encryption (min. 8) » "))
-                print(Style.RESET_ALL)
-                key_check = str(getpass(self.time_hm + Fore.GREEN + " [+] Wrote down AGAIN your Passphrase for Encryption (min. 8) » "))
-                print(Style.RESET_ALL)
-                if key == key_check:
-                    if len(key) >= 8: break
-                    else: continue
-                else: continue
-            # Return
-            if choice == '1':
-                message = str(input(self.time_hm + Fore.GREEN + " [+] Wrote down your hidden Textpassage » "))
-                print(Style.RESET_ALL)
-                return 'write', message, key
-            if choice == '2':
-                return 'read', None, key
+            if os.path.isfile(self.path): break
             else:
-                sys.exit(0)
+                print(self.time_hm + Fore.RED + " [-] File is not readable")
+                print(Style.RESET_ALL)
+                continue
+        # Passphrase for encryption
+        while True:
+            key = str(getpass(self.time_hm + Fore.GREEN + " [+] Wrote down your Passphrase for Encryption (min. 8) » "))
+            print(Style.RESET_ALL)
+            key_check = str(getpass(self.time_hm + Fore.GREEN + " [+] Wrote down AGAIN your Passphrase for Encryption (min. 8) » "))
+            print(Style.RESET_ALL)
+            if key == key_check:
+                if len(key) >= 8: break
+                else: continue
+            else: continue
+        # Return
+        if choice == '1':
+            message = str(input(self.time_hm + Fore.GREEN + " [+] Wrote down your hidden Textpassage » "))
+            print(Style.RESET_ALL)
+            return 'write', message, key, None
+        if choice == '2':
+            return 'read', None, key, None
+        else:
+            sys.exit(0)
+
+    def read_salt(self):
+
+        try:
+            if os.path.isfile("salt.pl"):
+                with open("salt.pl", 'rb') as f:
+                    self.salt = f.read()
+            elif not os.path.isfile("salt.pl"): self.gen_salt()
+        except PermissionError:
+            print("Permission Error")
+            sys.exit(0)
+
+    def gen_salt(self):
+
+        with open("salt.pl", 'wb') as f:
+            self.salt = Random.new().read(16)
+            f.write(self.salt)
+            print(self.time_hm + Fore.RED + " [+] SALT GENERATED")
+            print(Style.RESET_ALL)
 
     def read(self):
 
@@ -182,7 +205,9 @@ class PyStegano():
     def run(self):
 
         self.out()
-        op_mode, message, key = self.input()
+        self.read_salt()
+        op_mode, message, key, _salt = self.input()
+        if _salt: self.gen_salt()
         if op_mode == 'write':
             self.ciphertext = self.enc(key=key, message=message)
             _true = self.write(ciphertext=self.ciphertext)
